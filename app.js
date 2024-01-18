d1976ccac4e6dad163ba77e084b51543b8ff7d6f
ChumaMqeke <94187682+ChumaMqeke@users.noreply.github.com>
2024-01-18 09:02:36 +0200

    contacts

--- app.js
+++ app.js
@@ -55,3 +55,17 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     this.reset();
 });
 PageTransitions();
+
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
